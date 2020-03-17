d6194dcbd8193a770c90e1889695d2c134158119
Gagan Vishal <Gagan.vishal@momentummetropolitan.co.za>
2020-03-17 16:58:04 +0200

    Updated to swift version 5.0.

--- PasswordTestField/ViewController.swift
+++ PasswordTestField/ViewController.swift
@@ -7,23 +7,25 @@
 
 import UIKit
 
-let numberOfTextFields = 5   //Set your number of textfields here
-
-
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pinFileldsContainerView: PinContainerStackView!
+    
+    //MARK:- View Controller life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Set your height & width here
-        self.view.backgroundColor = UIColor(red: 62.0/256.0, green: 152.0/256.0, blue: 229.0/256.0, alpha: 1)
-        //Add This code
-        let pinView  = PinView(frame : CGRect(x: 0, y: 150, width: UIScreen.main.bounds.size.width, height: 100))
-        self.view.addSubview(pinView)
+        //
+        pinFileldsContainerView.registerDelegate = self
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+}
+
+
+//MARK: RegisterSecondaryDevice Delegate
+extension ViewController: RegisterSecondaryDevice {
+    func registerSecondaryDevice(with token: String) {
+           /*
+          send to server or process locally
+        */
+          print("token is :\(token)")
     }
 
 }
-
